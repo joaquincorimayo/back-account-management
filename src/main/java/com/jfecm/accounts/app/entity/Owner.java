6e9a8e403c2cd6f96b5608014b4330e5176c3459
joaquincorimayo <joaquin97corimayo@gmail.com>
2023-04-25 23:06:58 -0300

    Feat: transactions - deposit.

--- src/main/java/com/jfecm/accounts/app/entity/Owner.java
+++ src/main/java/com/jfecm/accounts/app/entity/Owner.java
@@ -3,10 +3,7 @@ package com.jfecm.accounts.app.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
